48b37d9d882f35cf45d0c95e0e1b0225991e5c47
Eugeny Ermakov <eermakov@payneteasy.com>
2019-09-27 20:50:45 +0300

    Added echo controller

---
+++ src/main/java/ngkbtr/controller/EchoController.java
@@ -0,0 +1,16 @@
+package ngkbtr.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class EchoController {
+
+    @RequestMapping(value = "/echo", method = RequestMethod.GET)
+    @ResponseBody
+    public Object echo(){
+        return "echo";
+    }
+}
